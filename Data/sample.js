2dbe09d71993fbf1b272db875ce6111241b1ec4f
Bongomin Daniel <bongomindaniel@gmail.com>
2023-07-21 09:45:31 +0300

    add data to the array

--- Data/sample.js
+++ Data/sample.js
@@ -5,7 +5,8 @@ const posts = [
         creatorName: "John Doe",
         postImage: "src/assets/image2.jpg",
         socialMediaIcon: "https://example.com/facebook-icon.png",
-        postBody: "This is the first post. Hello, world!",
+        postBody:
+            "This is the first post. Hello, world! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend sapien a justo facilisis, nec dignissim libero malesuada. Cras elementum, elit at suscipit luctus, urna magna viverra odio, vitae luctus metus orci in enim. Proin hendrerit purus ut tellus venenatis, et lobortis risus porttitor. Duis nec enim eleifend, vulputate ligula eu, vulputate neque. Aenean a ligula eget urna volutpat tincidunt. Sed vitae ipsum eget odio cursus venenatis. Nulla id arcu at turpis sagittis pellentesque. Aliquam pharetra turpis et quam malesuada pharetra. Vivamus non facilisis quam, in commodo neque.",
         deleted: false,
         liked: 56,
     },
@@ -15,7 +16,8 @@ const posts = [
         creatorName: "Jane Smith",
         postImage: "src/assets/image1.jpg",
         socialMediaIcon: "https://example.com/twitter-icon.png",
-        postBody: "Hey, here's my second post. Have a great day!",
+        postBody:
+            "Hey, here's my second post. Have a great day! Duis vel congue odio. Etiam consectetur blandit nunc, sit amet fermentum augue malesuada in. Nunc fringilla massa nec neque faucibus, ut varius massa cursus. Pellentesque congue dapibus erat, vel pellentesque nisi facilisis ac. In hac habitasse platea dictumst. Donec pellentesque, justo et venenatis blandit, quam mi semper sapien, a gravida purus neque nec enim. Maecenas at dignissim enim. In hac habitasse platea dictumst.",
         deleted: false,
         liked: 102,
     },
@@ -25,7 +27,8 @@ const posts = [
         creatorName: "Bob Johnson",
         postImage: "src/assets/image2.jpg",
         socialMediaIcon: "https://example.com/instagram-icon.png",
-        postBody: "This is the third post. Feeling blessed!",
+        postBody:
+            "This is the third post. Feeling blessed! Curabitur venenatis aliquet fermentum. Pellentesque convallis sem ac dui varius, vel aliquam sapien dignissim. Etiam nec tristique arcu. Pellentesque eu massa ac eros bibendum venenatis ac nec elit. Duis tincidunt ex eu nulla scelerisque tincidunt. Integer id sagittis nisl, vitae lacinia eros. Sed vitae interdum nulla. Nullam nec enim sed purus interdum aliquet. Aenean id hendrerit arcu.",
         deleted: true,
         liked: 28,
     },
