f781d5bab069deab23fada0ae571cafde0d2b890
Bongomin Daniel <bongomindaniel@gmail.com>
2023-07-22 13:26:25 +0300

    add discussion button

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,13 +1,79 @@
 <script setup>
 import { ref } from "vue";
 import { useSidebar } from "../composables/useSidebar";
+import TestTree from "../components/testTree.vue";
 
 const collapsed = ref({
   1: true,
 });
 
+// New data property for the interests and tree
+const interests = ref([
+  { id: 1, name: "Food", tree: [] },
+  { id: 2, name: "Music", tree: [] },
+  { id: 3, name: "Movies", tree: [] },
+  { id: 4, name: "News", tree: [] },
+]);
+
 function toggleCollapse(collapseId) {
   collapsed.value[collapseId] = !collapsed.value[collapseId];
+}
+
+async function updateTree(interest, index) {
+  if (index === 0) {
+    if (interest.tree.length === 0) {
+      if (interest.name === "Music") {
+        // Simulate adding subcategories to the tree.
+        interest.tree.push({ id: 1, name: "pop", subTree: [] });
+        interest.tree.push({ id: 2, name: "Electronic", subTree: [] });
+        interest.tree.push({ id: 3, name: "Rock", subTree: [] });
+        interest.tree.push({ id: 1, name: "Hip-hop / Rap", subTree: [] });
+        interest.tree.push({ id: 2, name: "Country", subTree: [] });
+        interest.tree.push({ id: 3, name: "Classical", subTree: [] });
+      } else if (interest.name === "Food") {
+        // Simulate adding subcategories to the tree.
+        interest.tree.push({ id: 1, name: "StreetFood", subTree: [] });
+        interest.tree.push({ id: 2, name: "Snaks", subTree: [] });
+        interest.tree.push({ id: 3, name: "Beverages", subTree: [] });
+        interest.tree.push({ id: 1, name: "Desserts", subTree: [] });
+        interest.tree.push({ id: 2, name: "Side Dishes", subTree: [] });
+        interest.tree.push({ id: 3, name: "Breakfast Dishes", subTree: [] });
+      } else if (interest.name === "Movies") {
+        interest.tree.push({ id: 1, name: "Actions", subTree: [] });
+        interest.tree.push({ id: 2, name: "Commedy", subTree: [] });
+        interest.tree.push({ id: 3, name: "Drama", subTree: [] });
+        interest.tree.push({ id: 101, name: "Science Fiction", subTree: [] });
+        interest.tree.push({ id: 102, name: "Fantasy", subTree: [] });
+        interest.tree.push({ id: 103, name: "Animation", subTree: [] });
+      } else if (interest.name === "News") {
+      } else {
+        // Simulate adding subcategories to the tree.
+        interest.tree.push({ id: 101, name: "Subcategory 1", subTree: [] });
+        interest.tree.push({ id: 102, name: "Subcategory 2", subTree: [] });
+        interest.tree.push({ id: 103, name: "Subcategory 3", subTree: [] });
+      }
+    } else {
+      // Simulate clearing the subcategories when unchecked.
+      interest.tree = [];
+    }
+  } else {
+    const selectedTreeItem = interest.subTree;
+    selectedTreeItem.push({
+      id: 201,
+      name: "Subcategory 1,2",
+      miniTree: [],
+    });
+    selectedTreeItem.push({
+      id: 202,
+      name: "Subcategory 1,2",
+      miniTree: [],
+    });
+    selectedTreeItem.push({
+      id: 203,
+      name: "Subcategory 1,2",
+      miniTree: [],
+    });
+  }
 }
 
 const { isOpen } = useSidebar();
@@ -167,12 +233,7 @@ const inactiveClass =
                 viewBox="0 0 330 330"
                 xml:space="preserve"
               >
-                <path
-                  id="XMLID_225_"
-                  d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
-	c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
-	s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
-                />
+                <!-- SVG path data for the arrow icon ... -->
               </svg>
               <svg
                 v-show="collapsed[1]"
@@ -186,91 +247,79 @@ const inactiveClass =
                 viewBox="0 0 511.735 511.735"
                 xml:space="preserve"
               >
-                <g>
-                  <g>
-                    <path
-                      d="M508.788,371.087L263.455,125.753c-4.16-4.16-10.88-4.16-15.04,0L2.975,371.087c-4.053,4.267-3.947,10.987,0.213,15.04
-			c4.16,3.947,10.667,3.947,14.827,0l237.867-237.76l237.76,237.76c4.267,4.053,10.987,3.947,15.04-0.213
-			C512.734,381.753,512.734,375.247,508.788,371.087z"
-                    />
-                  </g>
-                </g>
+                <!-- SVG path data for the arrow icon ... -->
               </svg>
             </span>
           </button>
           <div
             v-if="collapsed[1]"
-            class="p-4 bg-white border-t border-gray-200"
+            class="p-2 bg-white border-t border-gray-200"
           >
-            <!-- Search Field -->
-            <div class="relative mb-4">
-              <input
-                type="text"
-                class="w-full px-4 py-2 pr-8 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
-                placeholder="Search intrest..."
-              />
-              <div class="absolute inset-y-0 right-0 flex items-center pr-3">
-                <svg
-                  class="w-5 h-5 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
+            <!-- Search Field ... -->
+
+            <!-- Checkbox Items -->
+            <div class="check-boxes py-1">
+              <template
+                v-for="interest in interests"
+                :key="'interest-' + interest.id"
+              >
+                <label
+                  class="flex py-2 items-center space-x-2 cursor-pointer border rounded px-4 my-2"
+                  :for="'checkbox-' + interest.id"
                 >
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 5.293a1 1 0 011.414 0L10 9.586l4.293-4.293a1 1 0 111.414 1.414L11.414 11l4.293 4.293a1 1 0 01-1.414 1.414L10 12.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 11 4.293 6.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
+                  <input
+                    type="checkbox"
+                    :id="'checkbox-' + interest.id"
+                    class="form-checkbox text-indigo-600 focus:ring focus:ring-indigo-500 h-4 w-4"
+                    @change="updateTree(interest, 0)"
                   />
-                </svg>
-              </div>
-            </div>
-            <div class="check-boxes m-2 py-3">
-              <!-- Checkbox Items -->
-              <label
-                class="flex py-2 items-center space-x-2 cursor-pointer"
-                for="checkbox-1"
-              >
-                <input
-                  type="checkbox"
-                  id="checkbox-1"
-                  class="text-indigo-600 rounded focus:ring focus:ring-indigo-500"
-                />
-                <span class="text-gray-700">Academics</span>
-              </label>
-              <label
-                class="flex py-1 items-center space-x-2 cursor-pointer"
-                for="checkbox-2"
-              >
-                <input
-                  type="checkbox"
-                  id="checkbox-2"
-                  class="text-indigo-600 rounded focus:ring focus:ring-indigo-500"
-                />
-                <span class="text-gray-700">Photoelectrics</span>
-              </label>
-              <label
-                class="flex py-1 items-center space-x-2 cursor-pointer"
-                for="checkbox-3"
-              >
-                <input
-                  type="checkbox"
-                  id="checkbox-3"
-                  class="text-indigo-600 rounded focus:ring focus:ring-indigo-500"
-                />
-                <span class="text-gray-700">Majoritarainism</span>
-              </label>
-              <label
-                class="flex py-1 items-center space-x-2 cursor-pointer"
-                for="checkbox-3"
-              >
-                <input
-                  type="checkbox"
-                  id="checkbox-3"
-                  class="text-indigo-600 rounded focus:ring focus:ring-indigo-500"
-                />
-                <span class="text-gray-700">Horror</span>
-              </label>
+                  <span class="text-gray-700">{{ interest.name }}</span>
+                </label>
+
+                <!-- Render the tree items under each interest -->
+                <ul class="ml-6 list-disc">
+                  <li
+                    v-for="treeItem in interest.tree"
+                    :key="'tree-' + treeItem.id"
+                    class="flex py-2 items-center space-x-2"
+                  >
+                    <input
+                      type="checkbox"
+                      :id="'checkbox-' + treeItem.id"
+                      class="form-checkbox text-indigo-600 focus:ring focus:ring-indigo-500 h-4 w-4"
+                      @change="updateTree(treeItem, 1)"
+                    />
+                    <label
+                      :for="'checkbox-' + treeItem.id"
+                      class="text-gray-700"
+                    >
+                      {{ treeItem.name }}
+                    </label>
+                    <!-- sub item -->
+                    <!-- <ul class="ml-6 list-disc">
+                      <li
+                        v-for="item in treeItem.subTree"
+                        :key="'tree-' + item.id"
+                        class="flex py-2 items-center space-x-2"
+                      >
+                        <input
+                          type="checkbox"
+                          :id="'checkbox-' + item.id"
+                          class="form-checkbox text-indigo-600 focus:ring focus:ring-indigo-500 h-4 w-4"
+                          @change="updateTree(item, 1)"
+                        />
+                        <label
+                          :for="'checkbox-' + item.id"
+                          class="text-gray-700"
+                        >
+                          {{ item.name }}
+                        </label>
+                      </li>
+                    </ul> -->
+                  </li>
+                </ul>
+              </template>
+
               <div class="more-filters mt-4 text-xs text-custom">
                 <a href="#">View 50 more ....</a>
               </div>
@@ -283,7 +332,7 @@ const inactiveClass =
             class="flex text-white items-center justify-between w-full px-4 py-2 font-medium text-left bg-custom-500 hover:bg-custom-600 focus:outline-none shadow-md"
             @click="toggleCollapse(1)"
           >
-            Source
+            Emotion / Mood
             <span>
               <svg
                 fill="#FFFFFF"
@@ -393,7 +442,7 @@ const inactiveClass =
             class="flex text-white items-center justify-between w-full px-4 py-2 font-medium text-left bg-custom-500 hover:bg-custom-600 focus:outline-none shadow-md"
             @click="toggleCollapse(1)"
           >
-            Land Mark
+            Place / Land Mark
             <span>
               <svg
                 fill="#FFFFFF"
@@ -448,7 +497,63 @@ const inactiveClass =
             class="flex text-white items-center justify-between w-full px-4 py-2 font-medium text-left bg-custom-500 hover:bg-custom-600 focus:outline-none shadow-md"
             @click="toggleCollapse(1)"
           >
-            College
+            College /Company
+            <span>
+              <svg
+                fill="#FFFFFF"
+                v-show="!collapsed[1]"
+                height="15px"
+                width="215x"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 330 330"
+                xml:space="preserve"
+              >
+                <path
+                  id="XMLID_225_"
+                  d="M325.607,79.393c-5.857-5.857-15.355-5.858-21.213,0.001l-139.39,139.393L25.607,79.393
+	c-5.857-5.857-15.355-5.858-21.213,0.001c-5.858,5.858-5.858,15.355,0,21.213l150.004,150c2.813,2.813,6.628,4.393,10.606,4.393
+	s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"
+                />
+              </svg>
+              <svg
+                v-show="hidden"
+                fill="#FFFFFF"
+                height="15px"
+                width="15px"
+                version="1.1"
+                id="Layer_1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlns:xlink="http://www.w3.org/1999/xlink"
+                viewBox="0 0 511.735 511.735"
+                xml:space="preserve"
+              >
+                <g>
+                  <g>
+                    <path
+                      d="M508.788,371.087L263.455,125.753c-4.16-4.16-10.88-4.16-15.04,0L2.975,371.087c-4.053,4.267-3.947,10.987,0.213,15.04
+			c4.16,3.947,10.667,3.947,14.827,0l237.867-237.76l237.76,237.76c4.267,4.053,10.987,3.947,15.04-0.213
+			C512.734,381.753,512.734,375.247,508.788,371.087z"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </span>
+          </button>
+          <div v-if="hiddent" class="p-4 bg-white border-t border-gray-200">
+            Collapsable Content
+          </div>
+        </div>
+
+        <!-- Collapsible 3 -->
+        <div class="collapsables">
+          <button
+            class="flex text-white items-center justify-between w-full px-4 py-2 font-medium text-left bg-custom-500 hover:bg-custom-600 focus:outline-none shadow-md"
+            @click="toggleCollapse(1)"
+          >
+            Date / Time STamp
             <span>
               <svg
                 fill="#FFFFFF"
---
+++ src/components/testTree.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <label>
+      <input type="checkbox" v-model="isChecked" @change="toggleTree" />
+      Show Categories
+    </label>
+    <div v-if="isChecked" class="tree">
+      <ul>
+        <li v-for="category in categories" :key="category.id">
+          {{ category.name }}
+          <ul>
+            <li v-for="item in category.items" :key="item.id">
+              {{ item.name }}
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isChecked: false,
+      categories: [
+        {
+          id: 1,
+          name: 'Category 1',
+          items: [
+            { id: 11, name: 'Item 1.1' },
+            { id: 12, name: 'Item 1.2' },
+          ],
+        },
+        {
+          id: 2,
+          name: 'Category 2',
+          items: [
+            { id: 21, name: 'Item 2.1' },
+            { id: 22, name: 'Item 2.2' },
+          ],
+        },
+        // Add more categories and items as needed
+      ],
+    };
+  },
+  methods: {
+    toggleTree() {
+      // You can perform any additional actions here when the checkbox is clicked.
+      // For now, we're just toggling the display of the tree.
+      this.isChecked = !this.isChecked;
+    },
+  },
+};
+</script>
+
+<style>
+/* Add your custom styles here */
+.tree {
+  margin-top: 10px;
+}
+</style>
