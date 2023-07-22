823536b8d6707df34060cd90887747d65d5374ea
Bongomin Daniel <bongomindaniel@gmail.com>
2023-07-22 13:35:23 +0300

    add search field

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -251,6 +251,28 @@ const inactiveClass =
               </svg>
             </span>
           </button>
+           <div class="relative mb-4">
+              <input
+                type="text"
+                class="w-full px-4 py-2 pr-8 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-indigo-500"
+                placeholder="Search interest..."
+              />
+              <div class="absolute inset-y-0 right-0 flex items-center pr-3">
+                <svg
+                  class="w-5 h-5 text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.293 5.293a1 1 0 011.414 0L10 9.586l4.293-4.293a1 1 0 111.414 1.414L11.414 11l4.293 4.293a1 1 0 01-1.414 1.414L10 12.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 11 4.293 6.707a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
+        </div>
           <div
             v-if="collapsed[1]"
             class="p-2 bg-white border-t border-gray-200"
