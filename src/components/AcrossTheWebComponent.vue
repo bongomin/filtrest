50eb3ce2a7f33e6697d4de87c0f236e98daa578c
Bongomin Daniel <bongomindaniel@gmail.com>
2023-07-23 20:14:17 +0300

    AcrossTheWebComponent

---
+++ src/components/AcrossTheWebComponent.vue
@@ -0,0 +1,24 @@
+
+<script setup>
+</script>
+
+<template>
+  <div class="main-div">
+    <h1 class="text-2xl font-bold mb-6">Across the web</h1>
+    <ul class="space-y-4">
+      <!-- Example list of news items -->
+      <li class="border-b border-gray-300 pb-4">
+        <h2 class="text-2xl font-semibold text-blue-600">Headline 1</h2>
+        <p class="text-gray-600">Published on: Date</p>
+        <p class="text-gray-600">Source: Source Name</p>
+        <p class="text-gray-800">Brief summary of the news item.</p>
+      </li>
+      <li class="border-b border-gray-300 pb-4">
+        <h2 class="text-2xl font-semibold text-blue-600">Headline 2</h2>
+        <p class="text-gray-600">Published on: Date</p>
+        <p class="text-gray-600">Source: Source Name</p>
+        <p class="text-gray-800">Brief summary of the news item.</p>
+      </li>
+    </ul>
+  </div>
+</template>
