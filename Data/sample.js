19c4f789f433ba27a90279979c815b64e8f25ef0
Bongomin Daniel <bongomindaniel@gmail.com>
2023-07-23 21:59:37 +0300

    change image src

--- Data/sample.js
+++ Data/sample.js
@@ -3,7 +3,7 @@ const posts = [
         createdAt: "10 mins",
         postersImage: "https://my-profile-murex-five.vercel.app/img/profile1.8e973a6d.jpeg",
         creatorName: "Daniel Danny",
-        postImage: "src/assets/image2.jpg",
+        postImage: "https://images.pexels.com/photos/3756523/pexels-photo-3756523.jpeg?auto=compress&cs=tinysrgb&w=800",
         socialMediaIcon: "https://example.com/facebook-icon.png",
         postBody:
             "This is the first post. Hello, world! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eleifend sapien a justo facilisis, nec dignissim libero malesuada. Cras elementum, elit at suscipit luctus, urna magna viverra odio, vitae luctus metus orci in enim. Proin hendrerit purus ut tellus venenatis, et lobortis risus porttitor. Duis nec enim eleifend, vulputate ligula eu, vulputate neque. Aenean a ligula eget urna volutpat tincidunt. Sed vitae ipsum eget odio cursus venenatis. Nulla id arcu at turpis sagittis pellentesque. Aliquam pharetra turpis et quam malesuada pharetra. Vivamus non facilisis quam, in commodo neque.",
@@ -15,7 +15,7 @@ const posts = [
         postersImage: "src/assets/profile1.jpeg",
         creatorName: "Jane Danny",
         postImage: "src/assets/image1.jpg",
-        socialMediaIcon: "https://example.com/twitter-icon.png",
+        socialMediaIcon: "https://images.pexels.com/photos/3758133/pexels-photo-3758133.jpeg?auto=compress&cs=tinysrgb&w=800",
         postBody:
             "Hey, here's my second post. Have a great day! Duis vel congue odio. Etiam consectetur blandit nunc, sit amet fermentum augue malesuada in. Nunc fringilla massa nec neque faucibus, ut varius massa cursus. Pellentesque congue dapibus erat, vel pellentesque nisi facilisis ac. In hac habitasse platea dictumst. Donec pellentesque, justo et venenatis blandit, quam mi semper sapien, a gravida purus neque nec enim. Maecenas at dignissim enim. In hac habitasse platea dictumst.",
         deleted: false,
@@ -25,7 +25,7 @@ const posts = [
         createdAt: "60 mins",
         postersImage: "https://my-profile-murex-five.vercel.app/img/profile1.8e973a6d.jpeg",
         creatorName: "Bob Johnson",
-        postImage: "src/assets/image2.jpg",
+        postImage: "https://images.pexels.com/photos/4087609/pexels-photo-4087609.jpeg?auto=compress&cs=tinysrgb&w=800",
         socialMediaIcon: "https://example.com/instagram-icon.png",
         postBody:
             "This is the third post. Feeling blessed! Curabitur venenatis aliquet fermentum. Pellentesque convallis sem ac dui varius, vel aliquam sapien dignissim. Etiam nec tristique arcu. Pellentesque eu massa ac eros bibendum venenatis ac nec elit. Duis tincidunt ex eu nulla scelerisque tincidunt. Integer id sagittis nisl, vitae lacinia eros. Sed vitae interdum nulla. Nullam nec enim sed purus interdum aliquet. Aenean id hendrerit arcu.",
