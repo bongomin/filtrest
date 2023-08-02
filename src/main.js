f8f3b67804656a4fa85e79ddd1de53499109613d
Bongomin Daniel <bongomindaniel@gmail.com>
2023-08-02 16:52:41 +0300

    add pinia

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia'; // Import createPinia from Pinia
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const pinia = createPinia(); // Initialize Pinia and get the Pinia instance
+
+const app = createApp(App);
+app.use(pinia); // Use the Pinia instance in the app
+
+app.mount('#app');
